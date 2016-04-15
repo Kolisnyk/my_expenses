d5f39e5cd4bbfce2fdfe9423bafb0082ccb99caa
Oleksii <Kolisnyk@inbox.ru>
2016-04-15 18:06:33 +0300

    Expenses v.1.5

--- expenses_reader.rb
+++ expenses_reader.rb
@@ -26,7 +26,7 @@ amount_by_day = Hash.new
 
 doc.elements.each("expenses/expense") do |item|
   loss_sum = item.attributes["amount"].to_i
-  loss_date= item.attributes["date"]
+  loss_date= Date.parse(item.attributes["date"])
   amount_by_day[loss_date] ||= 0
   amount_by_day[loss_date] += loss_sum
 end
@@ -37,15 +37,14 @@ sum_by_month = Hash.new
 current_month = amount_by_day.keys.sort[0].strftime("%B %Y")
 
 amount_by_day.keys.sort.each do |key|
-
   sum_by_month[key.strftime("%B %Y")] ||=0
   sum_by_month[key.strftime("%B %Y")] += amount_by_day[key]
 end
 puts "-------[ #{current_month}, всего потрачено: #{sum_by_month[current_month]} грн. ]-------"
  amount_by_day.keys.sort.each do |key|
-   if  key.strftime ("%B %Y") !=current_month
+   if  key.strftime("%B %Y") !=current_month
      current_month = key.strftime("%B %Y")
-     puts "------#{current_month}, всего потрачено: #{sum_by_month[current_month]} грн. ]-------"
+     puts "------[ #{current_month}, всего потрачено: #{sum_by_month[current_month]} грн. ]-------"
    end
    puts "\t#{key.day}: #{amount_by_day[key]} грн."
  end
